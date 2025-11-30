ef5605546838a5f0054253fa1649d61225f49427
Shovan61 <shovonmazumder61@gmail.com>
2025-11-30 21:56:28 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Button, Card } from "antd";
+
+export default function Home() {
+  return (
+    <Card>
+      <Button type="primary">Button</Button>{" "}
+    </Card>
+  );
+}
